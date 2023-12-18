c015ca4a01a8967e88a151f5ff635ab6a13b4dc1
Léo Hilaire <contact@leohilaire.com>
2023-12-18 11:49:29 +0100

    ➕ Add DaisyUI dependency

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,53 @@
+import daisyui from 'daisyui';
 import type { Config } from 'tailwindcss';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [daisyui],
+	daisyui: {
+		themes: [
+			{
+				light: {
+					primary: '#085852',
+
+					secondary: '#00ad9c',
+
+					accent: '#00d6bc',
+
+					neutral: '#383838',
+
+					'base-100': '#FFFFFF',
+
+					info: '#5CA3E6',
+
+					success: '#00d6bc',
+
+					warning: '#F2DB69',
+
+					error: '#FA3F19'
+				},
+				dark: {
+					primary: '#085852',
+
+					secondary: '#00ad9c',
+
+					accent: '#00d6bc',
+
+					neutral: '#ffffff',
+
+					'base-100': '#323232',
+
+					info: '#5CA3E6',
+
+					success: '#00d6bc',
+
+					warning: '#F2DB69',
+
+					error: '#FA3F19'
+				}
+			}
+		]
+	}
 } satisfies Config;
