b04a7b5874ac0bb0a74890b6d2bf9add307829a9
Nekxio <contact@leohilaire.com>
2024-01-09 15:10:15 +0100

    ➕ Add DayJS dependency

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,13 @@
 import type { Locales } from '$i18n/i18n-types';
 import { loadLocaleAsync } from '$i18n/i18n-util.async';
+import dayjs from 'dayjs';
+import 'dayjs/locale/en';
+import 'dayjs/locale/fr';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 import type { LayoutLoad } from './$types';
 export const load: LayoutLoad<{ locale: Locales }> = async ({ data: { locale } }) => {
 	await loadLocaleAsync(locale);
+	dayjs.extend(LocalizedFormat);
+	dayjs.locale(locale);
 	return { locale };
 };
