a83b36df9ab48c8d36a7143bb25cc13f3c6699ef
Nekxio <contact@leohilaire.com>
2024-03-16 16:23:15 +0100

    ♻️ Refactor Dashboard Profile page to add formType

--- src/routes/[lang=lang]/account/dashboard/profile/+page.svelte
+++ src/routes/[lang=lang]/account/dashboard/profile/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import DashboardForm from '$lib/components/dashboard/DashboardForm.svelte';
+	import LL from '$i18n/i18n-svelte';
 
 	export let data;
 	const { customer } = data;
-	const formType = 'Profile';
+	const formType = 'updateProfile';
 </script>
 
-<DashboardForm {customer} {formType} />
+<div class="lg:min-h-screen flex flex-col justify-center space-y-6">
+	<h1 class="text-3xl font-bold">{$LL.account.dashboard.profile.title()}</h1>
+	<p>{$LL.account.dashboard.profile.description()}</p>
+	<DashboardForm {customer} {formType} />
+</div>
