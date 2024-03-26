34c55c9a4637b07ed55a542b37605e28582111c5
Nekxio <contact@leohilaire.com>
2024-03-26 16:18:32 +0100

    🎨 Updated layout to add cart information in the user infos

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,22 @@
 import medusa from '$lib/server/medusa';
-import type { CustomerDTO } from '@medusajs/types';
+import type { Cart, CustomerInfos, Order } from '../types/medusa';
 import type { LayoutServerLoad } from './$types';
+
 export const load: LayoutServerLoad = async ({
 	locals,
 	cookies
-}): Promise<{ locale: string; user: CustomerDTO | undefined }> => {
-	let user: CustomerDTO | undefined;
+}): Promise<{ locale: string; user: CustomerInfos | undefined }> => {
+	let user: CustomerInfos | undefined;
 	const userData = await medusa.getCustomer(locals, cookies);
-	userData ? (user = userData) : (user = undefined);
+	if (userData) {
+		const orders: Awaited<Order>[] = await Promise.all(
+			userData.orders.map((order: Order) => medusa.getOrder(locals, order.id))
+		);
+		const cart: Awaited<Cart> = await medusa.getCart(locals, cookies);
+		user = { ...userData, orders: orders, cart: cart };
+	} else {
+		user = undefined;
+	}
 	const locale = locals.locale;
 	return { locale, user };
 };
