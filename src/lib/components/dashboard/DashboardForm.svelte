6a2bdaa13d5adff0905b9cd74fbf8e8561e9c5cf
Nekxio <contact@leohilaire.com>
2024-03-31 19:08:16 +0200

    🎨 Update DashboardForm component to add billing address in Checkout page

--- src/lib/components/dashboard/DashboardForm.svelte
+++ src/lib/components/dashboard/DashboardForm.svelte
@@ -4,15 +4,53 @@
 	import { enhance } from '$app/forms';
 	import type { Address, Customer } from 'sveltekit-medusa-client';
 	import { authorizedCountries } from '$lib/constants';
+	import { page } from '$app/stores';
+	import { createEventDispatcher } from 'svelte';
+	import { browser } from '$app/environment';
 
 	export let customer: Customer | undefined = undefined;
 	export let address: Address | undefined = undefined;
-	export let formType: 'updateProfile' | 'newAddress' | 'updateAddress';
+	export let formType: 'updateProfile' | 'newAddress' | 'updateAddress' | 'billingAddress';
 
 	let isSending: boolean = false;
 	let isFormSent: boolean = false;
 	let isError: boolean = false;
 	let addressCountry: string = address ? address.country_code.toUpperCase() : '';
+	let isCheckout: boolean = $page.url.pathname.includes('checkout');
+
+	export let isFormValid: boolean = false;
+	const dispatch = createEventDispatcher();
+
+	function validateCheckoutBillingAddressForm() {
+		if (browser) {
+			const requiredInputs = Array.from(
+				document.querySelectorAll(
+					`form[name='billingAddress'] input[required], form[name='billingAddress'] select[required]`
+				)
+			);
+			const isValid = requiredInputs.every((input) => {
+				if (input instanceof HTMLInputElement || input instanceof HTMLSelectElement) {
+					return input.value.trim() !== '';
+				}
+				return false;
+			});
+			if (isValid) {
+				const allInputs = Array.from(
+					document.querySelectorAll(`form[name='billingAddress'] input, form[name='billingAddress'] select`)
+				);
+				const formValues = allInputs.reduce((values: Record<string, string>, input) => {
+					if ((input instanceof HTMLInputElement || input instanceof HTMLSelectElement) && input.name !== '') {
+						values[input.name] = input.value;
+					}
+					return values;
+				}, {});
+				dispatch('formValid', (isFormValid = true));
+				dispatch('formValues', formValues);
+			} else {
+				dispatch('formValid', (isFormValid = false));
+			}
+		}
+	}
 </script>
 
 <form
@@ -21,6 +59,7 @@
 	action={`?/${formType}`}
 	enctype="multipart/form-data"
 	class="flex flex-col space-y-6 max-w-2xl"
+	on:change={validateCheckoutBillingAddressForm}
 	use:enhance={() => {
 		isSending = true;
 		return async ({ update, result }) => {
@@ -89,7 +128,7 @@
 		</div>
 	{/if}
 
-	{#if formType === 'newAddress' || formType === 'updateAddress'}
+	{#if formType === 'newAddress' || formType === 'updateAddress' || formType === 'billingAddress'}
 		<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
 			<div class="w-full">
 				<label for="first_name" class="label">
@@ -130,7 +169,6 @@
 					placeholder={$LL.account.dashboard.addresses.company()}
 					class="input input-bordered w-full"
 					value={address ? address.company : ''}
-					required
 				/>
 			</div>
 			<div class="w-full col-span-2">
@@ -202,7 +240,7 @@
 				<label for="country" class="label">
 					<span class="label-text">{$LL.account.dashboard.addresses.country()}</span>
 				</label>
-				<select class="select select-bordered w-full" name="country" bind:value={addressCountry}>
+				<select class="select select-bordered w-full" name="country" bind:value={addressCountry} required>
 					<option value="" selected disabled>{$LL.account.dashboard.addresses.selectCountry()}</option>
 					{#each authorizedCountries as country}
 						<option value={country.code}>{country.name}</option>
@@ -211,51 +249,53 @@
 			</div>
 		</div>
 	{/if}
-	<div class="self-center lg:self-end flex flex-row items-center space-x-4">
-		{#if isError}
-			<p>{$LL.account.dashboard.error()}</p>
-		{/if}
-		<button type="submit" class="btn btn-primary w-fit flex flex-row space-x-2">
-			{#if isSending}
-				{$LL.account.dashboard.updateInProgress()}
-				<svg
-					class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-				>
-					<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-					<path
-						class="opacity-75"
-						fill="currentColor"
-						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-					></path>
-				</svg>
+	{#if !isCheckout}
+		<div class="self-center lg:self-end flex flex-row items-center space-x-4">
+			{#if isError}
+				<p>{$LL.account.dashboard.error()}</p>
 			{/if}
-			{#if isFormSent}
-				{$LL.account.dashboard.updateCompleted()}
-				<svg
-					clip-rule="evenodd"
-					fill-rule="evenodd"
-					stroke-linejoin="round"
-					stroke-miterlimit="2"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-					class="w-6 fill-current"
-				>
-					<path
-						d="m11.998 2.005c5.517 0 9.997 4.48 9.997 9.997 0 5.518-4.48 9.998-9.997 9.998-5.518 0-9.998-4.48-9.998-9.998 0-5.517 4.48-9.997 9.998-9.997zm0 1.5c-4.69 0-8.498 3.807-8.498 8.497s3.808 8.498 8.498 8.498 8.497-3.808 8.497-8.498-3.807-8.497-8.497-8.497zm-5.049 8.886 3.851 3.43c.142.128.321.19.499.19.202 0 .405-.081.552-.242l5.953-6.509c.131-.143.196-.323.196-.502 0-.41-.331-.747-.748-.747-.204 0-.405.082-.554.243l-5.453 5.962-3.298-2.938c-.144-.127-.321-.19-.499-.19-.415 0-.748.335-.748.746 0 .205.084.409.249.557z"
-						fill-rule="nonzero"
-					/>
-				</svg>
-			{/if}
-			{#if !isSending && !isFormSent}
-				{#if formType === 'updateProfile' || (formType === 'updateAddress' && address)}
-					{$LL.account.dashboard.update()}
-				{:else}
-					{$LL.account.dashboard.add()}
+			<button type="submit" class="btn btn-primary w-fit flex flex-row space-x-2">
+				{#if isSending}
+					{$LL.account.dashboard.updateInProgress()}
+					<svg
+						class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+						<path
+							class="opacity-75"
+							fill="currentColor"
+							d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+						></path>
+					</svg>
 				{/if}
-			{/if}
-		</button>
-	</div>
+				{#if isFormSent}
+					{$LL.account.dashboard.updateCompleted()}
+					<svg
+						clip-rule="evenodd"
+						fill-rule="evenodd"
+						stroke-linejoin="round"
+						stroke-miterlimit="2"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+						class="w-6 fill-current"
+					>
+						<path
+							d="m11.998 2.005c5.517 0 9.997 4.48 9.997 9.997 0 5.518-4.48 9.998-9.997 9.998-5.518 0-9.998-4.48-9.998-9.998 0-5.517 4.48-9.997 9.998-9.997zm0 1.5c-4.69 0-8.498 3.807-8.498 8.497s3.808 8.498 8.498 8.498 8.497-3.808 8.497-8.498-3.807-8.497-8.497-8.497zm-5.049 8.886 3.851 3.43c.142.128.321.19.499.19.202 0 .405-.081.552-.242l5.953-6.509c.131-.143.196-.323.196-.502 0-.41-.331-.747-.748-.747-.204 0-.405.082-.554.243l-5.453 5.962-3.298-2.938c-.144-.127-.321-.19-.499-.19-.415 0-.748.335-.748.746 0 .205.084.409.249.557z"
+							fill-rule="nonzero"
+						/>
+					</svg>
+				{/if}
+				{#if !isSending && !isFormSent}
+					{#if formType === 'updateProfile' || (formType === 'updateAddress' && address)}
+						{$LL.account.dashboard.update()}
+					{:else}
+						{$LL.account.dashboard.add()}
+					{/if}
+				{/if}
+			</button>
+		</div>
+	{/if}
 </form>
