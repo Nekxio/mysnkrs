4d3b94ba50ec836984d57bbb3bb6cce20e3ea1cc
Nekxio <contact@leohilaire.com>
2024-04-07 11:06:46 +0200

    ✨ Add Checkout shipping selection page

---
+++ src/routes/[lang=lang]/checkout/shipping/+page.server.ts
@@ -0,0 +1,27 @@
+import medusa from '$lib/server/medusa';
+import { error } from '@sveltejs/kit';
+import type { Actions } from '../../../../../.svelte-kit/types/src/routes/[lang=lang]/checkout/addresses/$types';
+import type { ShippingOption } from '../../../../types/medusa';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals }): Promise<{ shippingOptions: ShippingOption[] }> => {
+	const shippingOptions: ShippingOption[] = (await medusa.getShippingOptions(locals)) as ShippingOption[];
+
+	return {
+		shippingOptions
+	};
+};
+
+export const actions: Actions = {
+	updateCartShippingOption: async ({ request, locals }) => {
+		const formData = await request.formData();
+		try {
+			const shippingOption: ShippingOption['id'] = String(formData.get('shippingOption'));
+
+			await medusa.selectShippingOption(locals, shippingOption);
+		} catch (e) {
+			console.error(e);
+			throw error(501, 'Failed to update shipping address');
+		}
+	}
+};
